1c8b58f123fa5be4cacca8f2caecfa7e51e36cbe
ivansmialko <ivansmyalko@gmail.com>
2023-06-04 17:23:41 +0300

    Додані нові звуки вибухів. Створені заново вибухові бочки, так щоб підтримувати інтерфейс IBulletHitInterface

---
+++ Paragon/Source/Paragon/Explosive.cpp
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Explosive.h"
+#include "Kismet/GameplayStatics.h"
+#include "Sound/SoundCue.h"
+#include "Particles/ParticleSystem.h"
+
+// Sets default values
+AExplosive::AExplosive()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AExplosive::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AExplosive::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AExplosive::BulletHit_Implementation(FHitResult HitResult)
+{
+	if (!ExplosionSound)
+		return;
+
+	UGameplayStatics::PlaySoundAtLocation(this, ExplosionSound, GetActorLocation());
+
+	if (!ExplosionParticles)
+		return;
+
+	UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), ExplosionParticles, HitResult.Location);
+
+	//TODO: Apply explosive damage
+
+	Destroy();
+}
+
